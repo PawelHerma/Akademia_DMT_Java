9b085da00b765d103f374457013c41b35acb9454
PawelHerma <pawel.slawomir.herma@gmail.com>
2022-12-28 17:02:07 +0100

    fix

--- Zajecia_10/Program.java
+++ Zajecia_10/Program.java
@@ -3,14 +3,7 @@ public class Program
     public static void main(String[] args)
     {
         Complex liczba = new Complex();
-        liczba.set(2,2);
-        String tekst = "3 + 5i";
-        Complex liczba_z_tekstu = new Complex();
-        liczba_z_tekstu.setFromText(tekst);
-        liczba_z_tekstu.print();
-        System.out.println( liczba_z_tekstu.mod() );
-        liczba_z_tekstu.conj().print();
-        System.out.print( "Suma dwoch liczb ");
-        Complex.add(liczba, liczba_z_tekstu).print();
+        liczba.set(2,-2);
+        liczba.print();
     }    
 }
