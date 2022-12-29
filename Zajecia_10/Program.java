35fad3e97efaad9147c65a90141a61e22aa78084
PawelHerma <pawel.slawomir.herma@gmail.com>
2022-12-29 12:42:43 +0100

    Update

--- Zajecia_10/Program.java
+++ Zajecia_10/Program.java
@@ -2,6 +2,10 @@ public class Program
 {
     public static void main(String[] args)
     {
+        Powtorka.liczDoFor(5);
+        boolean czyJest = Powtorka.jestPalindromem("oko");
+        System.out.print(czyJest);
+
         Complex liczba = new Complex();
         liczba.set(2,-2);
         liczba.print();
